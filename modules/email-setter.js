c2d9a67696c08437a1639ce633f99fec4703d124
xinove <jesusmagana1984@yahoo.es>
2015-06-23 23:41:53 +0200

    Entorno Establecido

---
+++ modules/email-setter.js
@@ -0,0 +1,8 @@
+module.exports = {
+	
+	host		: 'smtp.gmail.com',
+	user 		: 'jesus.magana.morales@gmail.com',
+	password 	: 'guaaa1984',
+	sender		: 'Jesús Magaña <jesus.magana.morales@gmail.com>'
+	
+}
